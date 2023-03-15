78518bbcecfa953e9537b0d44a3eca02b421a122
iamkubic <vighneshpradhan@gmail.com>
2023-03-15 13:42:03 +0530

    Initial Commit

---
+++ Registration-Screen-UI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Registration-Screen-UI
+//
+//  Created by Vighnesh Pradhan on 15/03/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
